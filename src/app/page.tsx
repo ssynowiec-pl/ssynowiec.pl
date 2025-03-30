e41b13b447c469bea7fd74807516d6ed04b02395
Stanisław Synowiec <stasyn1410@gmail.com>
2025-03-30 12:06:30 +0200

    chore(footer): replace current year text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+const currentYear = new Date().getFullYear();
+
 const Home = () => {
 	return (
 		<div className="flex h-svh flex-col">
@@ -5,7 +7,7 @@ const Home = () => {
 				<h1 className="text-3xl">Stanisław Synowiec | ssynowiec.pl</h1>
 			</main>
 			<footer className="flex justify-center">
-				<p>&copy; 2024 ssynowiec.pl. All rights reserved.</p>
+				<p>&copy; {currentYear} ssynowiec.pl. All rights reserved.</p>
 			</footer>
 		</div>
 	);
