bb3df98ede66c7d62d63e70bcdc7290283614866
Stanisław Synowiec <stasyn1410@gmail.com>
2025-12-04 08:59:12 +0100

    feat: compress HTML on prod

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  compressHTML: import.meta.env.PROD,
   vite: {
     plugins: [tailwindcss()],
   },
